1eb2f49d07d0a0048e0cbe9ee81ba1051df00f84
hiratayuki <hirata.yuki4050@gmail.com>
2020-02-16 12:08:06 +0900

    Initial commit

---
+++ web/js/practice.js
@@ -0,0 +1,12 @@
+var msg;
+var input;
+
+function initial() {
+  msg = document.getElementById('msg');
+  input = document.getElementById('input');
+  msg.textContent = '何か書いてください';
+}
+
+function doclick(e){
+  msg.textContent = '[' + input.value + ']と書きました';
+}
